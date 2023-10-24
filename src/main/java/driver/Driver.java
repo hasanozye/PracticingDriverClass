c9d6421ef3bd95bed437eca89366f2c07fb9575f
Hasan Özyer <hasanozyer06@gmail.com>
2023-10-24 22:26:49 +0300

    Implemented a Driver class for Selenium automation framework, providing methods to initialize and manage WebDriver instances for various browsers (Chrome, Firefox, Safari, Edge, IE). Utilized ThreadLocal to handle thread safety. Implemented a switch statement to create specific browser drivers based on input. Enhanced code readability using static imports from DriverFactory."

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -1,4 +1,42 @@
 package driver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static driver.DriverFactory.*;
+
 public class Driver {
+
+    private static ThreadLocal<WebDriver> drivers = new ThreadLocal<>();
+    private static ThreadLocal<WebDriverWait> waits = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+        return getDriver();
+    }
+
+    public static WebDriver getDriver(Browsers browser) {
+
+        if (drivers.get() == null) {
+            switch (browser) {
+                case IE -> {
+                    drivers.set(createIe());
+                }
+                case EDGE -> {
+                    drivers.set(createEdge());
+                }
+                case SAFARI -> {
+                    drivers.set(createSafari());
+                }
+                case FIREFOX -> {
+                    drivers.set(createFirefox());
+                }
+                default -> {
+                    drivers.set(createChrome());
+                }
+            }
+
+        }
+        return drivers.get();
+    }
+
 }
