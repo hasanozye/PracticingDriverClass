6700ced2314eaedf6d44cb51a80a6208b02b6615
Hasan Özyer <hasanozyer06@gmail.com>
2023-10-26 08:23:15 +0300

    added java documentation comments for the class

--- src/main/java/readers/property/PropertyReader.java
+++ src/main/java/readers/property/PropertyReader.java
@@ -1,22 +1,40 @@
 package readers.property;
 
-import com.beust.jcommander.StringKey;
-
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Properties;
 
+/**
+ * The "PropertyReader" class provides utility methods to read, modify, and store properties
+ * from a specified .properties file.
+ *
+ * Usage Example:
+ * '''
+ * PropertyReader reader = PropertyReader.read("custom_config");
+ * String value = reader.get("key");
+ * reader.set("newKey", "newValue");
+ * reader.store("Updated properties");
+ * '''
+ *
+ * @version 1.0
+ * @author [Hasan Özyer]
+ */
+
 public class PropertyReader {
 
     Properties properties = new Properties();
-    FileReader fileReader;
-    FileWriter fileWriter;
     String file;
 
+    /**
+     * Constructs a "PropertyReader" object by loading properties from the specified file.
+     *
+     * @param fileName The name of the .properties file (without extension) to be read.
+     * @throws RuntimeException If there is an error loading the properties file.
+     */
     public PropertyReader(String fileName) {
         try {
-            file = "src/main/resources/" + fileName + ".properties";
-            fileReader = new FileReader(file);
+            this.file = "src/main/resources/" + fileName + ".properties";
+            FileReader fileReader = new FileReader(file);
             properties.load(fileReader);
 
         } catch (Exception e) {
@@ -26,40 +44,67 @@ public class PropertyReader {
     }
 
     /**
-     * This is a static factory method that creates a PropertyReader object with a default file name ("config.properties").
-     * It provides a convenient way to create a PropertyReader without specifying a file name each time.
-     * @return
+     * Static factory method that creates a "PropertyReader" object with a default file name ("config.properties").
+     *
+     * @return A "PropertyReader" object initialized with default properties file.
      */
-
     public static PropertyReader read() {
         return read("config");
     }
 
+    /**
+     * Static factory method that creates a "PropertyReader" object with a specified file name.
+     *
+     * @param fileName The name of the .properties file (without extension) to be read.
+     * @return "PropertyReader" object initialized with the specified properties file.
+     */
     public static PropertyReader read(String fileName) {
         return new PropertyReader(fileName);
     }
 
+
+    /**
+     * Retrieves the value of a property associated with the given key.
+     *
+     * @param key The key of the property.
+     * @return The value of the property, or null if the key is not found.
+     */
     public String get(String key) {
         return properties.getProperty(key);
     }
 
-/*    public void set(String key, String value) {
+    /**
+     * Sets the value of a property identified by the specified key.
+     *
+     * @param key The key of the property.
+     * @param value The new value to be set for the property.
+     */
+    public void set(String key, String value) {
         properties.setProperty(key, value);
     }
 
+    /**
+     * Deletes a property identified by the specified key.
+     *
+     * @param key The key of the property to be removed.
+     */
     public void del(String key, String value) {
         properties.remove(key);
     }
 
+    /**
+     * Stores the properties back to the file with an optional comment.
+     *
+     * @param comment A comment to be written at the beginning of the properties file.
+     */
     public void store(String comment) {
         try {
-            fileWriter = new FileWriter(file);
+
+            FileWriter fileWriter = new FileWriter(file);
             properties.store(fileWriter, comment);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
- */
 
 }
